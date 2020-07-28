01349bd401af9e90e73d8bbb1460acde77327169
Aristide Manyessé <aristidemanyesse@gmail.com>
2020-07-28 09:53:24 +0000

    mise a jour

--- webapp/gestion/modules/master/vehicules/index.php
+++ webapp/gestion/modules/master/vehicules/index.php
@@ -18,6 +18,12 @@
             <div class="animated fadeInRightBig">
 
                 <div class="ibox">
+                    <div class="ibox-title">
+                        <h4>Lorem ipsum dolor sit amet.</h4>
+                        <div class="ibox-tools">
+                            
+                        </div>
+                    </div>
                     <div class="ibox-content">
                       <div class="row">
                         <?php foreach ($autos as $key => $auto) {
@@ -25,32 +31,31 @@
                             <div class="col-lg-3 col-md-4 col-sm-6 hoverable" style="margin-bottom: 1%">
                                 <div class="contact-box mp3">
                                     <a href="<?= $this->url("gestion", "master", "vehicule", $auto->id)  ?>">
-                                       <div class="row">
-                                        <div class="col-3">
-                                            <div class="text-center">
-                                                <img alt="image" class="rounded-circle m-t-xs img-fluid" src="<?= $this->stockage("images", "mecaniciens", $auto->image)  ?>">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <div class="text-center">
+                                                    <img alt="image" class="rounded-circle m-t-xs img-fluid" src="<?= $this->stockage("images", "mecaniciens", $auto->image)  ?>">
+                                                </div>
+                                            </div>
+                                            <div class="col-9">
+                                                <h3 class="text-capitalize"><strong><?= $auto->name() ?></strong></h3>
+                                                <p class="small mp0"><i class="fa fa-map-marker"></i> Lorem ipsum.</p>
+                                                <p class="small mp0"><i class="fa fa-phone"></i> Lorem ipsum dolor.</p>
+                                                <p class="small mp0"><i class="fa fa-envelope"></i>Lorem ipsum dolor.</p>
                                             </div>
                                         </div>
-                                        <div class="col-9">
-                                            <h3 class="text-capitalize"><strong><?= $auto->name() ?></strong></h3>
-                                            <p class="small mp0"><i class="fa fa-map-marker"></i> <?= $auto->adresse ?></p>
-                                            <p class="small mp0"><i class="fa fa-phone"></i> <?= $auto->contact ?></p>
-                                            <p class="small mp0"><i class="fa fa-envelope"></i> <?= $auto->email ?></p>
-                                        </div>
-                                    </div><hr class="mp3">
-                                    <div class="text-center text-uppercase">---> <strong><?= $auto->groupemecanicien->name() ?></strong> <---</div>
-                                </a>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    <?php } ?>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<?php include($this->rootPath("webapp/gestion/elements/templates/footer.php")); ?>
+    <?php include($this->rootPath("webapp/gestion/elements/templates/footer.php")); ?>
 
 
 </div>
